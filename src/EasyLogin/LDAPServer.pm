d1421fb9c01ff25a7a231b88c2704b57f948d3c4
Yoann Gini <yoann.gini@gmail.com>
2017-12-30 12:24:27 +0100

    Correctly return JSON value to LDAP request, with support for attribute filtering

--- src/EasyLogin/LDAPServer.pm
+++ src/EasyLogin/LDAPServer.pm
@@ -6,6 +6,7 @@ use warnings;
 use Data::Dumper;
 use Net::LDAP::Server;
 use Net::LDAP::Constant qw(LDAP_SUCCESS LDAP_OPERATIONS_ERROR LDAP_NO_SUCH_ATTRIBUTE LDAP_INVALID_SYNTAX LDAP_NO_SUCH_OBJECT LDAP_INVALID_CREDENTIALS LDAP_UNAVAILABLE LDAP_UNWILLING_TO_PERFORM);
+use Net::LDAP::Entry;
 use JSON;
 use REST::Client;
 use base 'Net::LDAP::Server';
@@ -28,9 +29,11 @@ sub restPostAtPathWithJSONObject {
 	
 	my $client = REST::Client->new();
 	
+	### TODO: use configuration system (CloudFoundry supported?) to find the REST API
 	my $request_url =  'http://127.0.0.1:8080/ldap/v1'.$path;
-	
-	$client->POST($request_url, $jsonBody, { "Content-type" => 'application/json'}); 
+
+	### TODO: if available, add the auth token in authentication headers
+	$client->POST($request_url, $jsonBody, { "Content-type" => 'application/json', "Accept" => 'application/json'}); 
 	return $client;
 }
 
@@ -78,23 +81,53 @@ sub search {
 	my $self = shift;
 	my ($reqData, $fullRequest) = @_;
 
+	print STDERR "New search request\n";
+
 	my @entries;
 
 	my $jsonRequest = encode_json $reqData;
-		print Dumper \$jsonRequest;
+
+	print STDERR "JSON Object for search request: $jsonRequest \n";
+
 	my $restClientWithAnswer = restPostAtPathWithJSONObject("/search", $jsonRequest);
-	
+
 	my $responseCode = $restClientWithAnswer->responseCode();
 
 	if ($responseCode == 200) {
+		print STDERR "Got result from EasyLogin Server\n";
 
-		print Dumper \$restClientWithAnswer;
+		print STDERR "JSON Object as search result: ".$restClientWithAnswer->responseContent()."\n";
+		
+		my $arrayOfRecords = decode_json $restClientWithAnswer->responseContent();
+		
+		foreach my $record (@$arrayOfRecords) {
+			my $entry = Net::LDAP::Entry->new;
+			$entry->dn($record->{dn});
+			delete $record->{dn};
+
+			if (exists $reqData->{"attributes"}) {
+				foreach my $key (@{$reqData->{"attributes"}}) {
+					foreach my $value ($record->{$key}) {
+						$entry->add ($key => $value);
+					}
+				}
+			} else {
+				foreach my $key (keys $record) {
+					foreach my $value ($record->{$key}) {
+						$entry->add ($key => $value);
+					}
+				}
+			}
+			push @entries, $entry;
+		}
 
 		return ldapReturnStruct(LDAP_SUCCESS), @entries;
 	} elsif ($responseCode == 401) {
+		print STDERR "Search denied by EasyLogin Server\n";
 		return ldapReturnStruct(LDAP_UNWILLING_TO_PERFORM);
 	}
 
+	print STDERR "Unexpected search error\n";
 	return ldapReturnStruct(LDAP_OPERATIONS_ERROR), @entries;
 }
 

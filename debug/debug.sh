ffc3d29a3383acd1f61fb2765b439c472969c02e
Yoann Gini <yoann.gini@gmail.com>
2018-01-08 14:29:24 +0100

    Removing debug scripts, useless now with lastly introduced docker run command that can also be used from a dev workstation

--- debug/debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
-
-perl -dt -I"${GIT_ROOT_DIR}/src" "${GIT_ROOT_DIR}/src/LDAPGateway.pl"
-
-exit $?
